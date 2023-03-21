a903d738b243a90ec7ea53376532e56d5c17b181
r0nz-29 <gs2019048@sgsitsindore.in>
2023-03-22 01:10:18 +0530

    + plain text content support

--- src/main/java/com/raunits/prettier/Formatter.java
+++ src/main/java/com/raunits/prettier/Formatter.java
@@ -3,42 +3,97 @@ package com.raunits.prettier;
 import java.util.Stack;
 
 public class Formatter {
-    public String format(String code) {
-        Stack<Pair> stack = new Stack<Pair>();
-        StringBuilder result = new StringBuilder();
-
+    public boolean valid(String code) {
+        Stack<String> stack = new Stack<>();
         char[] input = code.toCharArray();
-        int i=0;
-
-        for (int k=0; k<input.length; k++) {
-            if (input[k]=='<' || input[k]=='>' || Character.isWhitespace(input[k]))
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == '<' || input[i] == '>' || Character.isWhitespace(input[i]))
                 continue;
 
-            if (input[k]=='/') {
+            if (input[i] == '/') {
+                if (stack.isEmpty()) return false;
+                StringBuilder tag = new StringBuilder();
+                while (i < input.length && input[i] != '>') {
+                    tag.append(input[i++]);
+                }
+                if (!stack.pop().equals(tag.toString())) return false;
+                continue;
+            }
+
+            StringBuilder sb = new StringBuilder("/");
+            while (i < input.length && input[i] != '>') {
+                sb.append(input[i++]);
+            }
+
+            stack.push(sb.toString());
+        }
+        return stack.isEmpty();
+    }
+
+    public String format(String code) {
+        Stack<Pair> stack = new Stack();
+        StringBuilder result = new StringBuilder();
+        char last = '*';
+
+        char[] input = code.toCharArray();
+        int i = 0;
+
+        for (int k = 0; k < input.length; k++) {
+            if (Character.isWhitespace(input[k]))
+                continue;
+
+            if (input[k] == '<' || input[k] == '>') {
+                last = input[k];
+                continue;
+            }
+
+            if (input[k] == '/') {
                 i--;
                 StringBuilder tagname = new StringBuilder();
-                while (input[k] != '>') {
+                while (k < input.length && input[k] != '>') {
                     tagname.append(input[k++]);
                 }
                 stack.push(new Pair(tagname.toString(), i));
+                last = input[k];
                 k++;
                 continue;
             }
 
+//            if (k > 0 && input[k-1] == '>') {
+            if (k > 0 && last == '>') {
+                StringBuilder textContent = new StringBuilder();
+
+                while (input[k] != '<')
+                    textContent.append(input[k++]);
+
+
+                stack.push(new Pair(textContent.toString().trim(), stack.peek().indent + 1, "content"));
+                last = input[k];
+                continue;
+            }
+
+
             StringBuilder tagname = new StringBuilder();
-            while (input[k] != '>') {
+            while (k < input.length && input[k] != '>') {
                 tagname.append(input[k++]);
             }
             stack.push(new Pair(tagname.toString(), i));
             i++;
+            last = input[k];
         }
 
-        for (Pair p: stack) {
+        for (Pair p : stack) {
             int indent = 2 * p.indent;
-            String tag = "<" + p.tag + ">";
             StringBuilder sb = new StringBuilder();
             while (indent-- > 0) sb.append(" ");
-            result.append(sb.toString() + tag + "\n");
+
+            if (p.type == "tag") {
+                String tag = "<" + p.tag + ">";
+                result.append(sb.toString() + tag + "\n");
+            } else {
+                String content = p.tag;
+                result.append(sb.toString() + content + "\n");
+            }
         }
 
         return result.toString();
@@ -48,9 +103,17 @@ public class Formatter {
 class Pair {
     String tag;
     int indent;
+    String type;
 
     public Pair(String tag, int in) {
         this.tag = tag;
         this.indent = in;
+        this.type = "tag";
+    }
+
+    public Pair(String tag, int in, String type) {
+        this.tag = tag;
+        this.indent = in;
+        this.type = type;
     }
 }
--- src/main/java/com/raunits/prettier/Prettier.java
+++ src/main/java/com/raunits/prettier/Prettier.java
@@ -1,6 +1,8 @@
 package com.raunits.prettier;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -8,7 +10,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.FileWriter;
@@ -22,13 +27,12 @@ public class Prettier extends Application {
     private final TextArea displayArea = new TextArea();
     private final Formatter formatter = new Formatter();
     public static final VBox displayAreaContainer = new VBox();
+    private static Text status = new Text("Valid");
     private static boolean textareaVisible = true;
 
     @Override
     public void start(Stage stage) throws IOException {
         uiSetup();
-        addFormatBtn();
-
         Scene scene = new Scene(window);
         stage.setTitle("Prettier");
         stage.setScene(scene);
@@ -37,40 +41,39 @@ public class Prettier extends Application {
 
     @Override
     public void stop() {
-        try {
-            FileWriter fileWriter = new FileWriter(".noto");
-            String markdown = textArea.getText();
-            for (char c : markdown.toCharArray()) {
-                fileWriter.write(c);
-            }
-            fileWriter.close();
-            System.out.println("saved successfully");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            FileWriter fileWriter = new FileWriter(".noto");
+//            String markdown = textArea.getText();
+//            for (char c : markdown.toCharArray()) {
+//                fileWriter.write(c);
+//            }
+//            fileWriter.close();
+//            System.out.println("saved successfully");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     private void uiSetup() {
         String placeholder = "Enter XML here";
         textAreaContainer.getChildren().add(textArea);
         textAreaContainer.setFillWidth(true);
+        VBox.setVgrow(textArea, Priority.ALWAYS);
+        textArea.setWrapText(true);
+        textArea.setFont(Font.font("JetBrains Mono"));
+        textArea.setText(placeholder);
+//        addEventListener(textArea);
 
         displayAreaContainer.getChildren().add(displayArea);
         displayAreaContainer.setFillWidth(true);
-
-        VBox.setVgrow(textArea, Priority.ALWAYS);
         VBox.setVgrow(displayArea, Priority.ALWAYS);
-
-        textArea.setWrapText(true);
-        textArea.setFont(Font.font("JetBrains Mono"));
-
         displayArea.setWrapText(true);
         displayArea.setFont(Font.font("JetBrains Mono"));
+        displayArea.setText("Formatted code will be displayed here");
 
         responsivize(50, 50);
 
-        textArea.setText(placeholder);
-        displayArea.setText("Formatted code will be displayed here");
+        bottomBar();
 
         window.add(textAreaContainer, 0, 0);
         window.add(displayAreaContainer, 1, 0);
@@ -87,11 +90,34 @@ public class Prettier extends Application {
         GridPane.setVgrow(textAreaContainer, Priority.ALWAYS);
     }
 
-    public void addFormatBtn() {
+    public void addEventListener(TextArea textArea) {
+        textArea.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                String code = textArea.getText();
+                if (formatter.valid(code)) {
+                    status.setText("Valid");
+                    status.setFill(Color.GREEN);
+                }
+                else {
+                    status.setText("Invalid");
+                    status.setFill(Color.RED);
+                }
+            }
+        });
+    }
+
+    public void bottomBar() {
+        HBox bottomBar = new HBox(12);
+        bottomBar.setAlignment(Pos.CENTER);
+        addFormatBtn(bottomBar);
+        addStatusIndicator(bottomBar);
+        textAreaContainer.getChildren().add(bottomBar);
+    }
+
+    public void addFormatBtn(HBox bar) {
         Button showEditorBtn = new Button("format");
-        HBox buttonPane = new HBox(12, showEditorBtn);
-        buttonPane.setAlignment(Pos.CENTER_LEFT);
-        textAreaContainer.getChildren().add(buttonPane);
+        bar.getChildren().add(showEditorBtn);
 
         showEditorBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -101,6 +127,10 @@ public class Prettier extends Application {
                 displayArea.setText(prettyCode);
             }
         });
+    }
+
+    public void addStatusIndicator(HBox bar) {
+        bar.getChildren().add(status);
     }
 
     public static void main(String[] args) {
